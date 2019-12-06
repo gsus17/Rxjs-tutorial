493a09f2c806a0d6bbe7597b086a171d3dc5d39a
gsus17 <suarezmolina17@gmail.com>
2019-12-06 10:34:37 -0300

    Estructura base de un observable.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import {Observable} from 'rxjs';
+
+// Existen 2 formas de crear un observable.
+
+// const obs$= Observable.create();
+const obs$= new Observable<string>(subscriber=>{
+    subscriber.next('Hola');
+    subscriber.complete();
+});
 
 
-console.log('Hola Mundo!');
-
-
-
-
-
-
-
+// Se pasan 3 callbacks next, error y complete.
+obs$.subscribe(
+    (valor)=>console.log('Next: ',valor),
+    (error)=>console.warn(error),
+    ()=>console.log('Completado')
+);
